c5a9f660aafbfb03be6d3bc45db4e3bb0915ab9d
Ryan Elias <ryanelias4@gmail.com>
2023-02-22 15:49:05 -0500

    signinscreen

--- app/src/main/java/com/example/ist261_project1/MainActivity.java
+++ app/src/main/java/com/example/ist261_project1/MainActivity.java
@@ -12,37 +12,12 @@ public class MainActivity extends AppCompatActivity {
 
     EditText enteruser;
     EditText enterpass;
-    TextView usertitle;
-    TextView passtitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        enteruser = findViewById(R.id.enter_user);
-        usertitle = findViewById(R.id.user_title);
-
-        try {
-            enteruser.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-                @SuppressLint("SetTextI18n")
-                @Override
-                public void onFocusChange(View view, boolean userFocus) {
-                    if (userFocus) {
-                        usertitle.setText("Username");
-                        enteruser.setHint("");
-                    } else {
-                        usertitle.setText(" ");
-                        enteruser.setHint("Username");
-                    }
-                }
-            });
-        }
-
-        catch (Exception E)
-        {
-            usertitle.setText(E.toString());
-        }
 
     }
 
